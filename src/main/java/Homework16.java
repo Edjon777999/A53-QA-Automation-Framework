b6e371090eb14e015df88d8fb29287a3b3929444
Edjon777999 <Edjon777999@users.noreply.github.com>
2024-01-24 03:19:36 -0500

    f
    f

--- src/main/java/Homework16.java
+++ src/main/java/Homework16.java
@@ -2,18 +2,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class TestHomework16 {
+public class Homework16 {
 
     @Test
     public void registrationNavigation() {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\SeleniumWebDrivers\\ChromeDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\eduard\\FRAMEWORK FILES\\bdd-koel-automation\\node_modules\\chromedriver\\lib\\chromedriver");
         WebDriver driver = new ChromeDriver();
 
         // Navigate to the URL
         driver.get("https://qa.koel.app/");
 
 
+
+        // Close the browser
         driver.quit();
     }
 }
